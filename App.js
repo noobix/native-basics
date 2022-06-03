4021c60e7450f0505a20c8048bd070e4a0f11b81
Kelvin Kabute <skipgh@gmail.com>
2022-06-03 11:25:33 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Pict001 from './assets/downsize_200k_v1.jpg' 
+import { StyleSheet, Text, View, 
+  SafeAreaView, Image, TouchableOpacity,
+TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>This is my first react-native app</Text>
+      <Text>This is code at its best</Text>
+      <Image source = { Pict001 } style = {{height: 300, width: 400}}/>
+      <TouchableOpacity style = {{backgroundColor: "yellow"}}>
+        <Text style = {{ color: "black"}}>Click Me</Text>
+      </TouchableOpacity>
+      <StatusBar style = "auto" />
+    </SafeAreaView>
   );
 }
 
